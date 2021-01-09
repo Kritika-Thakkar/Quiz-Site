49d8f2b8d8859ea14f001380b7f81d324a7e8f3e
Kritika Thakkar <thakkarkritika2002@gmail.com>
2021-01-09 21:03:08 +0530

    Committed

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'this is question 1',
+        a: 'option 1',
+        b: 'option 2',
+        c: 'option 3',
+        d: 'option 4',
+        correct: 'c',
+    }, {
+        question: 'this is question 2',
+        a: 'option 1',
+        b: 'option 2',
+        c: 'option 3',
+        d: 'option 4',
+        correct: 'a',
+    }, {
+        question: 'this is question 3',
+        a: 'option 1',
+        b: 'option 2',
+        c: 'option 3',
+        d: 'option 4',
+        correct: 'd',
+    }, {
+        question: 'this is question 4',
+        a: 'option 1',
+        b: 'option 2',
+        c: 'option 3',
+        d: 'option 4',
+        correct: 'b',
+    }, {
+        question: 'this is question 5',
+        a: 'option 1',
+        b: 'option 2',
+        c: 'option 3',
+        d: 'option 4',
+        correct: 'c',
+    }
+]
