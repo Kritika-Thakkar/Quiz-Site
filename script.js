2f8073734c48363b06c0787ae3a4bf14a73b5b35
Kritika Thakkar <thakkarkritika2002@gmail.com>
2021-01-12 00:30:42 +0530

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quizData = [
     {
-        question: "When is Kritka's birthday?",
+        question: "When is Kritika's birthday?",
         a: 'June 11th',
         b: 'Alien?',
         c: 'Jan 27',
@@ -102,7 +102,7 @@ submitBtn.addEventListener('click', () => {
             loadQuiz();
         } else {
             quiz.innerHTML = `<h2>You have answered ${score} correctly out of ${quizData.length} questions.</h2>
-            <button onclick = "location.reload">Do it again?</button>`;
+            <button onclick = "location.reload()" >Do it again?</button>`;
         }
     }
 
