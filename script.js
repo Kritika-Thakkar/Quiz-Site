25c77b57a3f019d22ce86fdb5b1641ad48865690
Kritika Thakkar <thakkarkritika2002@gmail.com>
2021-01-12 00:27:48 +0530

    Completed.

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
 const quizData = [
     {
-        question: 'this is question 1',
-        a: 'option 1',
-        b: 'option 2',
-        c: 'option 3',
-        d: 'option 4',
+        question: "When is Kritka's birthday?",
+        a: 'June 11th',
+        b: 'Alien?',
+        c: 'Jan 27',
+        d: 'May 5',
         correct: 'c',
     }, {
-        question: 'this is question 2',
-        a: 'option 1',
-        b: 'option 2',
-        c: 'option 3',
-        d: 'option 4',
+        question: "What is Kritika's favourite color?",
+        a: 'Yellow',
+        b: 'None',
+        c: 'Green',
+        d: 'Black',
         correct: 'a',
     }, {
-        question: 'this is question 3',
-        a: 'option 1',
-        b: 'option 2',
-        c: 'option 3',
-        d: 'option 4',
+        question: 'What year is this?',
+        a: '2020',
+        b: '2022',
+        c: '1885',
+        d: '2021',
         correct: 'd',
     }, {
-        question: 'this is question 4',
-        a: 'option 1',
-        b: 'option 2',
-        c: 'option 3',
-        d: 'option 4',
+        question: 'Which school did Kritika go to?',
+        a: 'DPS',
+        b: 'PORPS',
+        c: 'KV-AFS',
+        d: 'Home schooled?',
         correct: 'b',
     }, {
-        question: 'this is question 5',
-        a: 'option 1',
-        b: 'option 2',
-        c: 'option 3',
-        d: 'option 4',
+        question: 'What does Kritika study right now?',
+        a: "She's married LOL",
+        b: 'Cooking',
+        c: 'Engineering',
+        d: 'Law',
         correct: 'c',
     }
 ];
 
 const questionEl = document.getElementById("question");
+const quiz = document.getElementById("quiz");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+const answerEls = document.querySelectorAll(".answer");
 
 let currentQuiz = 0;
+let score = 0;
+// let answer = undefined;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -61,21 +67,44 @@ function loadQuiz() {
 }
 
 function getSelected() {
-    const answers = document.querySelectorAll("answer");
-    answers.forEach((answer) => {
-        console.log(answer.value);
+    
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
     });
 }
 
-submitBtn.addEventListener('click', () => {
-    currentQuiz++;
 
-    if(currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        // show results
-        alert("The quiz is completed! Get yourself a lemonade!");
+submitBtn.addEventListener('click', () => {
+    
+    
+    const answer = getSelected();
+    console.log(answer);  //remove this line if you do not want a list of selected answers in the console 
+
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You have answered ${score} correctly out of ${quizData.length} questions.</h2>
+            <button onclick = "location.reload">Do it again?</button>`;
+        }
     }
 
-
+    
 });
